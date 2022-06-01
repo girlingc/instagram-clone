34be8475e775b4d9efba08c1ad78d5d763609927
Christian Girling <girling.christian@gmail.com>
2022-05-31 18:32:41 -0700

    Set up header component

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>I am a header</h1>
+
+      {/* Left */}
+
+      {/* Middle */}
+
+      {/* Right */}
+    </div>
+  );
+}
+
+export default Header;
