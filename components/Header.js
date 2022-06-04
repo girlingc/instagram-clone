7d2157b7fd51e25610054254e6bc6948f9dd9b24
Christian Girling <girling.christian@gmail.com>
2022-06-03 23:34:49 -0700

    Add search bar to middle

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
@@ -22,7 +30,16 @@ function Header() {
           />
         </div>
         {/* Middle */}
-
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
         {/* Right */}
       </div>
     </div>
