9a5f33cd2fd6f66d3a6e9be7f9df8f55b8adfb12
Christian Girling <girling.christian@gmail.com>
2022-06-03 23:25:13 -0700

    Add left side of header

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,27 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h1>I am a header</h1>
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
 
-      {/* Left */}
-      <div>
-        <Image src="https://links.papareact.com/ocw" layout="fill" />
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        {/* Middle */}
+
+        {/* Right */}
       </div>
-      {/* Middle */}
-
-      {/* Right */}
     </div>
   );
 }
