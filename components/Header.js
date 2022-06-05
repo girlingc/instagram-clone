e3e7e74b0c8c17bb658625e58201f010b312f6ae
Christian Girling <girling.christian@gmail.com>
2022-06-04 18:20:00 -0700

    Add global class css

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,12 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl lg:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid h-24 w-24">
           <Image
@@ -30,17 +31,24 @@ function Header() {
           />
         </div>
         {/* Middle */}
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 border-gray-300 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input
-            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:ring-black focus:border-black"
-            type="text"
-            placeholder="Search"
-          />
         </div>
         {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <PaperAirplaneIcon className="navBtn" />
+        </div>
       </div>
     </div>
   );
